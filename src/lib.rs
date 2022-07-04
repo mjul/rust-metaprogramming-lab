b613508b74c66355bdff2a66ac48bd1d98c9dc06
Martin Jul <martin@mjul.com>
2022-07-04 22:33:53 +0200

    Add DocString and some traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use std::clone::Clone;
+
+/// A documentation string
+#[derive(Debug, Clone)]
+struct DocString {
+    value: String,
+}
+impl DocString {
+    pub fn from(value: &str) -> Self {
+        DocString {
+            value: value.to_string(),
+        }
+    }
+}
+
 enum PrimitiveType {
     Id,
     String,
@@ -5,18 +20,31 @@ enum PrimitiveType {
 }
 
 enum FieldType {
-    Primitive(PrimitiveType)
+    Primitive(PrimitiveType),
 }
+
+/// Named represents a named entity
+trait Named {
+    fn get_name(&self) -> String;
+}
+
+/// Documented represents somehting with documentation, a docstring.
+trait Documented {
+    fn get_docstring(&self) -> DocString;
+}
+
+/// Declarations are named and documented
+trait Declaration: Named + Documented {}
 
 struct FieldDeclaration {
     name: String,
     field_type: FieldType,
-    docstring: String,
+    docstring: DocString,
     sort_index: usize,
 }
 
 impl FieldDeclaration {
-    fn new(name: String, tp: FieldType, docstr: String, sort_index: usize) -> Self {
+    fn new(name: String, tp: FieldType, docstr: DocString, sort_index: usize) -> Self {
         FieldDeclaration {
             name: name,
             field_type: tp,
@@ -26,21 +54,51 @@ impl FieldDeclaration {
     }
 }
 
+impl Named for FieldDeclaration {
+    fn get_name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+impl Documented for FieldDeclaration {
+    fn get_docstring(&self) -> DocString {
+        self.docstring.clone()
+    }
+}
+
+impl Declaration for FieldDeclaration {}
+
 /// Declaration for an event
 struct EventDeclaration {
     name: String,
+    docstring: DocString,
     fields: Vec<FieldDeclaration>,
 }
 
 impl EventDeclaration {
     // Create a new event
-    fn new(name: String, fields: Vec<FieldDeclaration>) -> Self {
+    fn new(name: String, docstring: DocString, fields: Vec<FieldDeclaration>) -> Self {
         EventDeclaration {
             name: name,
+            docstring: docstring,
             fields: fields,
         }
     }
 }
+
+impl Named for EventDeclaration {
+    fn get_name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+impl Documented for EventDeclaration {
+    fn get_docstring(&self) -> DocString {
+        self.docstring.clone()
+    }
+}
+
+impl Declaration for EventDeclaration {}
 
 /// The MetaModel is the main entry-point into the meta-model
 pub struct MetaModel {
@@ -57,37 +115,52 @@ impl MetaModel {
 pub fn new_example_model() -> MetaModel {
     MetaModel::new(vec![EventDeclaration::new(
         String::from("MonthEnded"),
+        DocString::from("This event signals that a calendar month has ended."),
         vec![
             FieldDeclaration::new(
                 String::from("id"),
                 FieldType::Primitive(PrimitiveType::Id),
-                String::from("Globally unique event-id"),
+                DocString::from("Globally unique event ID"),
                 1,
             ),
             FieldDeclaration::new(
                 String::from("description"),
                 FieldType::Primitive(PrimitiveType::Id),
-                String::from("Textual description of this event."),
+                DocString::from("Textual description of this event."),
                 2,
             ),
             FieldDeclaration::new(
                 String::from("first_day_in_month"),
                 FieldType::Primitive(PrimitiveType::LocalDate),
-                String::from("The first date in the month that ended."),
+                DocString::from("The first date in the month that ended."),
                 3,
             ),
             FieldDeclaration::new(
                 String::from("last_day_in_month"),
                 FieldType::Primitive(PrimitiveType::LocalDate),
-                String::from("The last date in the month that ended."),
+                DocString::from("The last date in the month that ended."),
                 4,
             ),
             FieldDeclaration::new(
                 String::from("first_day_in_next_month"),
                 FieldType::Primitive(PrimitiveType::LocalDate),
-                String::from("The first date in the following month."),
+                DocString::from("The first date in the following month."),
                 5,
             ),
         ],
     )])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_example_model_must_return_model() -> Result<(), String> {
+        let actual = new_example_model();
+        match actual.events.is_empty() {
+            false => Ok(()),
+            true => Err(String::from("Expected some event declarations")),
+        }
+    }
 }
