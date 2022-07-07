8046895b7152d627af0ab50775537893bfc8d8e1
Martin Jul <martin@mjul.com>
2022-07-07 12:24:27 +0200

    Add Debug trait to model

--- metamodel/src/lib.rs
+++ metamodel/src/lib.rs
@@ -1,31 +1,37 @@
-use std::clone::Clone;
-
+#[derive(Debug)]
 pub enum PrimitiveType {
     Id,
-    LocalDate
+    LocalDate,
 }
 
+#[derive(Debug)]
 pub enum Type {
-    Primitive(PrimitiveType)
+    Primitive(PrimitiveType),
 }
 
+#[derive(Debug)]
 pub enum Name {
-    Literal(String)
+    Literal(String),
 }
 
 /// Documentation
+#[derive(Debug)]
 pub struct Documentation {
     label: String,
     description: String,
 }
 
 impl Documentation {
-    pub fn new(label: &str, description: &str) -> Self { Self {label: label.to_string(), description: description.to_string() } }
+    pub fn new(label: &str, description: &str) -> Self {
+        Self {
+            label: label.to_string(),
+            description: description.to_string(),
+        }
+    }
 }
 
-
+#[derive(Debug)]
 pub enum Expr {
     FieldDeclarationExpr(Name, Documentation, Type),
-    RecordDeclarationExpr(Name, Documentation)
+    RecordDeclarationExpr(Name, Documentation),
 }
-
