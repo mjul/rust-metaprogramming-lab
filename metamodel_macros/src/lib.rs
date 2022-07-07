097538af76ff70c6ea902ed614d221255769d8f9
Martin Jul <martin@mjul.com>
2022-07-07 22:49:09 +0200

    Add code gen for RecordDeclaration

--- metamodel_macros/src/lib.rs
+++ metamodel_macros/src/lib.rs
@@ -224,6 +224,63 @@ mod tests {
     }
 }
 
+
+fn generate_code_for_meta_model(ast : metamodel::Expr) -> TokenStream {
+    println!("🚀🚀🚀 meta-model: {:?}", ast);
+
+    let code = match ast {
+        metamodel::Expr::RecordDeclarationExpr(rd) => match rd {
+            metamodel::RecordDeclaration {
+                name,
+                documentation: _,
+                fields,
+            } => match name {
+                metamodel::Name::Literal(name) => {
+                    let struct_ident: syn::Ident = syn::parse_str(&name).unwrap();
+                    let struct_path_ident : syn::Path = syn::parse_str(&name).unwrap();
+                    let struct_fields: syn::Fields = match fields.is_empty() {
+                        true => syn::Fields::Unit,
+                        false => {
+                            let mut pnames : syn::punctuated::Punctuated<syn::Field, syn::Token![,]> = syn::punctuated::Punctuated::new();
+                            for fd in fields.iter() {
+                                // TODO: take the field name
+                                let field_ident_path : syn::Path = syn::parse_str("foobar").unwrap();
+                                let field_expr_path : syn::ExprPath = syn::parse_str("i32").unwrap();
+                                let i32_path : syn::Path = syn::parse_str("i32").unwrap();
+
+                                // build FieldValue instance  { attrs, member, colon_token, expr }
+                                let fv_member_ident : syn::Ident = syn::parse_str("x").unwrap();
+                                //let fv_member = syn::Member::Named(fv_member_ident);
+                                let fv_colon = Some(syn::token::Colon::default());
+                                let fv_expr_path : syn::Path = syn::parse_str("i32").unwrap();
+                                //let fv_expr = syn::Expr::Path(syn::ExprPath {attrs:  vec![], qself: None, path: fv_expr_path});
+                                //let fv = syn::FieldValue { attrs: vec![], member:fv_member, colon_token: fv_colon, expr: fv_expr};
+
+                                let field : syn::Field = syn::Field { attrs:vec![],
+                                    vis:syn::Visibility::Public(syn::VisPublic {pub_token: syn::token::Pub::default()}),
+                                    ident: Some(fv_member_ident),
+                                    colon_token: fv_colon,
+                                    ty: syn::Type::Path(syn::TypePath { qself: None, path:fv_expr_path }),
+                                };
+                                pnames.push(field);
+
+                            }
+                            syn::Fields::Named(syn::FieldsNamed { brace_token: syn::token::Brace::default(), named: pnames })
+                        },
+                    };
+                    //let struct_expr = syn::ExprStruct { attrs: vec![], path: struct_path_ident, brace_token: syn::token::Brace::default(), fields: struct_fields };
+                    quote!( struct #struct_ident { #struct_fields } )
+                }
+            },
+        },
+        _ => todo!(),
+    };
+
+    println!("🚀🚀🚀 code: {:?}", code);
+
+    code.into()
+}
+
 #[proc_macro]
 pub fn generate_model_from_tuple(input: TokenStream) -> TokenStream {
     let item: syn::Expr = syn::parse(input).expect("failed to parse input");
@@ -285,6 +342,11 @@ pub fn generate_model_from_tuple(input: TokenStream) -> TokenStream {
                                                             let no_fields: Vec<
                                                                 metamodel::FieldDeclaration,
                                                             > = vec![];
+
+                                                            let one_field = vec![metamodel::FieldDeclaration::new(metamodel::Name::Literal(String::from("id")),
+                                                            metamodel::Documentation::new("ID", "The entity ID."),
+                                                            metamodel::Type::Primitive(metamodel::PrimitiveType::Id))];
+
                                                             metamodel::Expr::RecordDeclarationExpr(
                                                                 metamodel::RecordDeclaration::new(
                                                                     metamodel::Name::Literal(n),
@@ -294,6 +356,7 @@ pub fn generate_model_from_tuple(input: TokenStream) -> TokenStream {
                                                                             .unwrap(),
                                                                     ),
                                                                     no_fields,
+                                                                    //one_field,
                                                                 ),
                                                             )
                                                         }
@@ -303,7 +366,6 @@ pub fn generate_model_from_tuple(input: TokenStream) -> TokenStream {
                                                 _ => todo!(),
                                             }
                                         }
-                                        _ => todo!(),
                                     },
                                     _ => todo!(),
                                 }
@@ -320,25 +382,8 @@ pub fn generate_model_from_tuple(input: TokenStream) -> TokenStream {
     };
 
     println!("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 macro input parsing completed...");
-    println!("🚀🚀🚀 meta-model: {:?}", ast);
 
-    let code = match ast {
-        metamodel::Expr::RecordDeclarationExpr(rd) =>
-        match rd {
-            metamodel::RecordDeclaration { name, documentation:_ , fields:_ } =>
-            match name {
-                metamodel::Name::Literal(name) => {
-                    let struct_ident: syn::Ident = syn::parse_str(&name).unwrap();
-                    quote!( struct #struct_ident {} )
-                }
-            }
-        },
-        _ => todo!(),
-    };
-
-    println!("🚀🚀🚀 code: {:?}", code);
-
-    code.into()
+    generate_code_for_meta_model(ast)
 }
 
 /*
@@ -354,3 +399,17 @@ pub fn generate_model_from_tuple(input: TokenStream) -> TokenStream {
     }.into()
 
 */
+
+#[cfg(test)]
+mod playground_tests {
+    use syn::{parse_str};
+    #[test]
+    fn expr() {
+        let es : syn::ExprStruct = syn::parse_str("Foo { a: i32, b : usize }").unwrap();
+        dbg!(es);
+        //let p : syn::Path = syn::parse_str("foobar").unwrap();
+        //dbg!(p);
+        let fv : syn::FieldValue = syn::parse_str("id: i32").unwrap();
+        dbg!(fv);
+    }
+}
--- metamodel_test/src/lib.rs
+++ metamodel_test/src/lib.rs
@@ -42,7 +42,7 @@ mod tests {
         use super::super::*;
 
         #[test]
-        fn must_emit_data_structure_for_record_declaration() {
+        fn must_emit_data_structure_for_record_declaration_with_no_fields() {
             generate_model_from_tuple!((
                 "record",
                 [
@@ -53,7 +53,7 @@ mod tests {
                             ("label", "Foo Record"),
                             (
                                 "description",
-                                "A Foo is a very important entity with just and ID."
+                                "A Foo is a very important entity with no fields."
                             ),
                         ],
                     ),
@@ -65,5 +65,42 @@ mod tests {
 
             assert_eq!(true, true);
         }
+
+/*
+        #[test]
+        fn must_emit_data_structure_for_record_declaration_with_one_field() {
+            generate_model_from_tuple!((
+                "record",
+                [
+                    ("name", "Bar"),
+                    (
+                        "documentation",
+                        [
+                            ("label", "Bar Record"),
+                            (
+                                "description",
+                                "A Bar is a very important entity with just and ID."
+                            ),
+                        ],
+                    ),
+                ],
+                [
+                    ("name", "id"),
+                    (
+                        "documentation",
+                        [
+                            ("label", "ID"),
+                            ("description", "The unique Bar entity ID.")
+                        ]
+                    )
+                ]
+            ));
+
+            // If this compiles, we the struct has been generated
+            let _actual = Bar {id:1};
+
+            assert_eq!(1, actual.id);
+        }
+*/
     }
 }
